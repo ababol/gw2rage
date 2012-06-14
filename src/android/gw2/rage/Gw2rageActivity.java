3015c82b6647af00dc173e0d673ce528cbdc51d1
Fedonono <arnaud.babol@gmail.com>
2012-06-14 17:05:28 +0200

    Base

---
+++ src/android/gw2/rage/Gw2rageActivity.java
@@ -0,0 +1,13 @@
+package android.gw2.rage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Gw2rageActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
