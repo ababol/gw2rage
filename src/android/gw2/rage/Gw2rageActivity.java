236e5550123878a3f76ced254f099f03b06a4550
Francois DAMBRINE <artragis@gmail.com>
2012-06-16 18:19:07 +0200

    mise en place du menu.

--- src/android/gw2/rage/Gw2rageActivity.java
+++ src/android/gw2/rage/Gw2rageActivity.java
@@ -2,6 +2,11 @@ package android.gw2.rage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+//import android.app.ActionBar;
 
 public class Gw2rageActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +14,37 @@ public class Gw2rageActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+      //  ActionBar actionBar = getActionBar();
+	// add the custom view to the action bar
+//	actionBar.setCustomView(R.layout.actionbar_view);
+	//actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM| ActionBar.DISPLAY_SHOW_HOME);
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.mainmenu, menu);
+            return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menuitem1:
+			Toast.makeText(this, R.string.status, Toast.LENGTH_SHORT)
+					.show();
+                        /**
+                         * @TODO show here the score, number of members... of the guild
+                         */
+			break;
+		case R.id.menuitem2:
+			Toast.makeText(this, R.string.curr_event, Toast.LENGTH_SHORT)
+					.show();
+			break;
+
+		default:
+			break;
+		}
+
+		return true;
+	}
+
 }
