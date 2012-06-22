bf4e171108331d2c94feb0c42aa04789e9a331a5
Francois DAMBRINE <artragis@gmail.com>
2012-06-22 17:38:45 +0200

    merge branch reseau -> master

--- src/android/gw2/rage/Gw2rageActivity.java
+++ src/android/gw2/rage/Gw2rageActivity.java
@@ -2,12 +2,66 @@ package android.gw2.rage;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+import org.apache.http.client.*;
+import org.apache.http.client.methods.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.impl.client.DefaultHttpClient;
+//import android.app.ActionBar;
 
 public class Gw2rageActivity extends Activity {
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+      //  ActionBar actionBar = getActionBar();
+	// add the custom view to the action bar
+//	actionBar.setCustomView(R.layout.actionbar_view);
+	//actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM| ActionBar.DISPLAY_SHOW_HOME);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.mainmenu, menu);
+            return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case 0x7f04002c/*R.id.menuitem1*/:
+			Toast.makeText(this, R.string.status, Toast.LENGTH_SHORT)
+					.show();
+                        try{
+                            String lien = "http://www.gw2rage.com/appli/status.php";//check the name
+                            HttpClient client = new DefaultHttpClient();
+                            HttpGet request = new HttpGet(lien);
+                            HttpResponse response = client.execute(request);
+                          
+                            Toast.makeText(this, response.toString(), Toast.LENGTH_LONG)
+					.show();
+                        }catch(Exception e){
+                            //@TODO voir les problèmes générés par URL
+                        }
+                        setContentView(R.layout.status);
+                        
+                        /**
+                         * @TODO show here the score, number of members... of the guild
+                         */
+			break;
+		/*case R.id.menuitem2:
+			Toast.makeText(this, R.string.curr_event, Toast.LENGTH_SHORT)
+					.show();
+			break;
+*/
+		default:
+			break;
+		}
+
+		return true;
 	}
+
 }
